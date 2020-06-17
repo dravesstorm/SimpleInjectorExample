412e8370520d66da62b902b09494aee74e945831
Sergey Brisov <60529562+dravesstorm@users.noreply.github.com>
2020-06-17 19:19:19 +0300

    Add files via upload

---
+++ Task2.DI/DI.Task/src/DI/DI.App/Program.cs
@@ -0,0 +1,43 @@
+﻿using DI.App.Abstractions;
+using DI.App.Abstractions.BLL;
+using DI.App.Models;
+using DI.App.Services;
+using DI.App.Services.PL;
+using DI.App.Services.PL.Commands;
+using SimpleInjector;
+using System.Collections.Generic;
+
+namespace DI.App
+{
+    internal class Program
+    {
+
+        private static readonly Container container;
+
+        static Program()
+        {
+            // 1. Create a new Simple Injector container
+            container = new Container();
+
+            // 2. Configure the container (register)
+            container.Register<IDatabaseService, InMemoryDatabaseService>(Lifestyle.Singleton);
+            container.Register<IUserStore, UserStore>();
+            container.Register<IUser, User>();
+            container.Register<ICommandProcessor, CommandProcessor>();
+            container.Collection.Register<ICommand>(typeof(AddUserCommand), typeof(ListUsersCommand));
+            container.Register<CommandManager>();
+
+
+            // 3. Verify your configuration
+            container.Verify();
+        }
+
+        private static void Main()
+        {
+            // Inversion of Control
+            var manager = container.GetInstance<CommandManager>();
+
+            manager.Start();
+        }
+    }
+}
---
+++ Task2.DI/DI.Task/src/DI/DI.App/Services/PL/Commands/CommandProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DI.App.Abstractions;
+using DI.App.Abstractions.BLL;
+using DI.App.Services.PL.Commands;
+
+namespace DI.App.Services.PL
+{
+    public class CommandProcessor : ICommandProcessor
+    {
+        private readonly Dictionary<int, ICommand> commands = new Dictionary<int, ICommand>();
+        public IEnumerable<ICommand> Commands => this.commands.Values.AsEnumerable();
+
+        public CommandProcessor(IEnumerable<ICommand> commands)
+        {
+            foreach (var item in commands)
+            {
+                this.commands.Add(item.Number, item);
+            }
+        }
+
+        public void Process(int number)
+        {
+            if (!this.commands.TryGetValue(number, out var command)) return;
+
+            command.Execute();
+        }
+
+        
+    }
+}
